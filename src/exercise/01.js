6193643641328de91d43cea74e750033a40083e9
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-07-21 20:33:28 +0200

    chore: made function reusable

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,11 +3,8 @@
 
 import * as React from 'react'
 
-const Globe = React.lazy(() => import('../globe'))
-
-const loadGlobe = () => {
-  import('../globe')
-}
+const loadGlobe = () => import('../globe')
+const Globe = React.lazy(loadGlobe)
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
