933ef6d0235bb347164d26964efbe824c48ee72b
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-07-22 14:35:44 +0200

    perf: added react memo to menu and listitems

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -30,7 +30,7 @@ function Menu({
     </ul>
   )
 }
-// 🐨 Memoize the Menu here using React.memo
+Menu = React.memo(Menu)
 
 function ListItem({
   getItemProps,
@@ -56,7 +56,7 @@ function ListItem({
     />
   )
 }
-// 🐨 Memoize the ListItem here using React.memo
+ListItem = React.memo(ListItem)
 
 function App() {
   const forceRerender = useForceRerender()
